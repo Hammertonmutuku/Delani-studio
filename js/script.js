236bb60beb7b7e06ea36fbfaa56ca23bca6cee95
Hammertonmutuku <lthamma3@gmail.com>
2019-11-04 12:23:12 +0300

    add text to the portfolios

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let submit = function(){
+    var name=document.getElementById("name");
+    var email=document.getElementById("email");
+    var message= document.getElementById("message");
+    if (email =""){
+        alert("Please enter your email")
+    }
+    else{
+        alert("Thank you for contacting us!" + " Your message has been received.")
+    }
+        
+    
+}

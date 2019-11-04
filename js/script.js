661cceb2a64544c8578e7c981eecd33a4431d413
Hammertonmutuku <lthamma3@gmail.com>
2019-11-04 13:02:15 +0300

    add hover effect on portfolio images

--- js/script.js
+++ js/script.js
@@ -8,6 +8,63 @@ let submit = function(){
     else{
         alert("Thank you for contacting us!" + " Your message has been received.")
     }
-        
-    
 }
+$(document).ready(function(){
+    $("#project1").hover(
+        function(){
+        $("#text1").show("slow");
+    },
+    function(){
+        $("#text1").hide("slow");
+    });
+    $("#project2").hover(
+        function(){
+        $("#text2").show("slow");
+    },
+    function(){
+        $("#text2").hide("slow");
+    });
+    $("#project3").hover(
+        function(){
+        $("#text3").show("slow");
+    },
+    function(){
+        $("#text3").hide("slow");
+    });
+    $("#project4").hover(
+        function(){
+        $("#text4").show("slow");
+    },
+    function(){
+        $("#text4").hide("slow");
+    });
+    $("#project5").hover(
+        function(){
+        $("#text5").show("slow");
+    },
+    function(){
+        $("#text5").hide("slow");
+    });
+    $("#project6").hover(
+        function(){
+        $("#text6").show("slow");
+    },
+    function(){
+        $("#text6").hide("slow");
+    });
+    $("#project7").hover(
+        function(){
+        $("#text7").show("slow");
+    },
+    function(){
+        $("#text7").hide("slow");
+    });
+    $("#project8").hover(
+        function(){
+        $("#text8").show("slow");
+    },
+    function(){
+        $("#text8").hide("slow");
+    });
+    
+});
